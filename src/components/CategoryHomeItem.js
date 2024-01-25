f8f48af7eec8c7a599c6d0eac3f473807099a28d
Mizanur Rahman <mizan@arogga.net>
2024-01-25 11:38:26 +0600

    fastlance setup for android

---
+++ src/components/CategoryHomeItem.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const CategoryHomeItem = () => {
+  return (
+    <View>
+      <Text>CategoryHomeItem</Text>
+    </View>
+  );
+};
+
+export default CategoryHomeItem;
+
+const styles = StyleSheet.create({});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,21 @@
-import {View, Text, Platform, StatusBar, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 import React from 'react';
 import Wrapper from '../components/Wrapper';
 import {getStatusBarHeight} from 'react-native-status-bar-height';
 import HomeHeader from '../components/HomeHeader';
+import CategoryHomeItem from '../components/CategoryHomeItem';
+import _ from 'lodash';
 
 const offSetheight = getStatusBarHeight();
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <Wrapper style={styles.wrapperStyle}>
       {Platform.OS === 'andnroid' && (
@@ -25,6 +34,42 @@ const Home = () => {
           left: 0,
         }}></View>
       <HomeHeader />
+      <View style={styles.categoryView}>
+        {!_.isEmpty() && (
+          <FlatList
+            data={_.get('categories', 'categories', [])}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => item.c_id.toString()}
+            style={{paddingHorizontal: 10}}
+            ListFooterComponent={
+              <CategoryHomeItem
+                name="All"
+                onPress={() => {
+                  navigation.navigate('SearchHealthcare', {
+                    cat_id: '0',
+                    category: null,
+                    title: 'All Categories',
+                  });
+                }}
+              />
+            }
+            renderItem={({item}) => (
+              <CategoryHomeItem
+                name={item.c_name}
+                img_url={item.c_img}
+                onPress={() => {
+                  navigation.navigate('SearchHealthcare', {
+                    cat_id: item.c_id,
+                    category: null,
+                    title: item.c_name,
+                  });
+                }}
+              />
+            )}
+          />
+        )}
+      </View>
     </Wrapper>
   );
 };
